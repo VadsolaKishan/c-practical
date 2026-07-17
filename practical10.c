f704437fb1628c3028be6bb8cfc5a767037e0aea
VadsolaKishan <vadsolakishan1310@gmail.com>
2026-07-17 19:21:52 +0530

    Add Practical 10

---
+++ practical10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+int i,a;
+printf("\nEnter The Number : ");
+scanf("%d",&a);
+for(i=a;i>=1;i--)
+{
+printf("\n%d",i);
+sleep(1);
+}
+
+printf("\n\nName : Vadsola Kishan Dhirubhai");
+printf("\nId : 24CE134");
+
+return 0;
+}
+
