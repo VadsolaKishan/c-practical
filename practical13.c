5a70c8f76f6d81e0a97ffef715263696b28d97b1
VadsolaKishan <vadsolakishan1310@gmail.com>
2026-07-17 19:21:53 +0530

    Add Practical 13

---
+++ practical13.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int i,j,ans;
+for(i=1;i<=10;i++)
+{
+    for(j=1;j<=5;j++)
+    {
+        ans = i * j;
+        printf("|%d\t",ans);
+    }
+    printf("\n");
+}
+
+printf("\n\nName : Vadsola Kishan Dhirubhai");
+printf("\nId : 24CE134");
+
+return 0;
+}
+
+
+
