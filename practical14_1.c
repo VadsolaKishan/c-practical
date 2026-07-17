47c791e70e5d1ef9cdcd8d2493f4079f85637f71
VadsolaKishan <vadsolakishan1310@gmail.com>
2026-07-17 19:21:53 +0530

    Add Practical 14

---
+++ practical14_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+for(i=1;i<=5;i++)
+{
+    for(j=1;j<=i;j++)
+    if(j%2!=0)
+    {
+        printf("\t1");
+    }
+    else
+    {
+        printf("\t0");
+    }
+    printf("\n");
+}
+
+printf("\n\nName : Vadsola Kishan Dhirubhai");
+printf("\nId : 24CE134");
+
+return 0;
+}
---
+++ practical14_3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,l,n,a;
+
+    printf("Enter The Number : ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++)
+    {
+        a = i -1;
+        for(int k=1;k<=n-i;k++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++)
+        {
+            printf("%c",j+64);
+        }
+        for(int l=1;l<=i-1;l++)
+        {
+            printf("%c",a+64);
+            a--;
+        }
+        printf("\n");
+    }
+    printf("\n\nName : Vadsola Kishan Dhirubhai");
+    printf("\nId : 24CE134");
+        return 0;
+}
+
+
+
