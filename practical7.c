6fb09e7314a7da3730f9a097f07b736139114118
VadsolaKishan <vadsolakishan1310@gmail.com>
2026-07-17 19:21:52 +0530

    Add Practical 7

---
+++ practical7.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+long int rb=12000000,rp=6000000,rc=3000000,rtw;
+long int sa=11000000,sh=8000000,sc=8000000,stw;
+rtw = rb + rp + rc;
+stw = sa + sh + sc;
+printf("\nraman total wealth : %ld",rtw);
+printf("\nsuman total wealth : %ld",stw);
+if(rtw>stw)
+{
+printf("\nraman is rechest : ");
+}
+else
+{
+printf("\nsuman is rechest : ");
+}
+
+printf("\n\nName : Vadsola Kishan Dhirubhai");
+printf("\nId : 24CE134");
+
+return 0;
+}
+
+
