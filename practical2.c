68556c0fc06fe27639d3c33835e4a837bcb21285
VadsolaKishan <vadsolakishan1310@gmail.com>
2026-07-17 19:21:51 +0530

    Add Practical 2

---
+++ practical2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,r_area;
+    printf("Enter The Length : ");
+    scanf("%d",&l);
+    printf("Enter The breadth : ");
+    scanf("%d",&b);
+    r_area = l * b;
+    printf("Area Of Rectangle : %d",r_area);
+
+    printf("\nName : Vadsola Kishan Dhirubhai");
+    printf("\nId : 24CE134");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
