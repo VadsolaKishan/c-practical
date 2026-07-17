da51180ba208fb36cb63d94bb91d6cc66c2cc598
VadsolaKishan <vadsolakishan1310@gmail.com>
2026-07-17 19:21:52 +0530

    Add Practical 5

---
+++ practical5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int c,f;
+printf("Enter Celsiuis : ");
+scanf("%d",&c);
+f = (c*9/5)+32;
+printf("Fahrenheit : %d",f);
+
+printf("\n\nName : Vadsola Kishan Dhirubhai");
+printf("\nId : 24CE134");
+
+return 0;
+}
