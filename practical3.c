1d21edb229d1a5b8549750446da32b641ff95570
VadsolaKishan <vadsolakishan1310@gmail.com>
2026-07-17 19:21:52 +0530

    Add Practical 3

---
+++ practical3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int si,p=50000,r=5.5,t=3;
+si = p*r*t/100;
+printf("simple interest : %d",si);
+
+printf("\n");
+
+printf("\nName : Vadsola Kishan Dhirubhai");
+printf("\nId : 24CE134");
+
+return 0;
+}
